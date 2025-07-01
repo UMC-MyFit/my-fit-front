db9060d858d3622f450594671d538ab27661b251
potatodev-777 <potatodev777@gmail.com>
2025-07-01 14:53:10 +0900

    feat: bottom nav add svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import BottomNav from "./components/layouts/BottomNav";
+
 function App() {
-  return <div className="font-sans max-w-md mx-auto w-full min-h-screen"></div>;
+  return (
+    <div className="font-sans max-w-md mx-auto w-full min-h-screen">
+      <BottomNav />
+    </div>
+  );
 }
 
 export default App;
