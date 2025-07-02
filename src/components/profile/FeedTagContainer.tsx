b600ba90d875579f4fbeb5c147ec14ce3420f2fc
potatodev-777 <potatodev777@gmail.com>
2025-07-02 18:36:45 +0900

    create required pages and component

---
+++ src/components/profile/FeedTagContainer.tsx
@@ -0,0 +1,26 @@
+import FeedTagItem from "./FeedTagItem";
+
+const tagList = [
+  "태그1",
+  "서비스기획",
+  "태그3",
+  "페르소나설정",
+  "태그5",
+  "태그6",
+  "태그7",
+  "태그8",
+  "태그9",
+  "태그10",
+];
+
+function FeedTagContainer() {
+  return (
+    <div className="gap-[5px] flex flex-wrap">
+      {tagList.map((tag, index) => (
+        <FeedTagItem key={index} tag={tag} />
+      ))}
+    </div>
+  );
+}
+
+export default FeedTagContainer;
