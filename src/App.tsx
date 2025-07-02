178dd8c34b0df5d903f26ace0f747d986ef4ce27
Minsu Kwon <kms37480@naver.com>
2025-07-02 22:25:03 +0900

    feat: 리크루팅 페이지 내에 저장된 공고 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-import SavedAnnouncement from "./pages/recruiting/SavedAnnouncement";
-
 function App() {
-  return (
-    <div className="font-sans max-w-md mx-auto w-full min-h-screen">
-      <SavedAnnouncement />
-    </div>
-  );
+  return <div className="font-sans max-w-md mx-auto w-full min-h-screen"></div>;
 }
 
 export default App;
