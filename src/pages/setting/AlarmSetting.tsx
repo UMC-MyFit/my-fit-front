ce6a8efd80033e4aeb114153c61deae9a7557637
Minsu Kwon <kms37480@naver.com>
2025-07-03 22:24:18 +0900

    beforerebase

---
+++ src/pages/setting/AlarmSetting.tsx
@@ -0,0 +1,15 @@
+import TopBar from "../../components/common/TopBar";
+
+function AlarmSetting() {
+  return (
+    <div className="flex flex-col">
+      <TopBar>
+        <span className="text-h2 font-Pretendard text-ct-black-100">
+          알림 설정
+        </span>
+      </TopBar>
+      <div className="mx-[19px]"></div>
+    </div>
+  );
+}
+export default AlarmSetting;
