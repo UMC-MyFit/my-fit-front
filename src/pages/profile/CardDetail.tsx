ff9e2e6eda63d8c57caaf8ef12aca12f64b1de44
potatodev-777 <potatodev777@gmail.com>
2025-07-04 15:16:21 +0900

    feat/modal initial commit

--- src/pages/profile/CardDetail.tsx
+++ src/pages/profile/CardDetail.tsx
@@ -1,6 +1,8 @@
 import DetailIntroduction from "../../components/profile/DetailIntroduction";
 import DetailCardItem from "../../components/profile/DetailCardItem";
 import TopBarContainer from "../../components/common/TopBarContainer";
+import Modal from "../../components/ui/Modal";
+import { useState } from "react";
 
 const TopBarContent = () => {
   return (
@@ -14,15 +16,24 @@ const TopBarContent = () => {
 
 function CardDetail() {
   const createList = Array.from({ length: 10 }, (_, i) => i + 1);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
     <TopBarContainer TopBarContent={<TopBarContent />}>
+      <div className="w-full h-[100px]" onClick={() => setIsModalOpen(true)}>
+        open modal
+      </div>
       <div className="w-full h-full bg-ct-gray-100 flex flex-col gap-[7px]">
         <DetailIntroduction />
         {createList.map((_, index) => (
           <DetailCardItem key={index} />
         ))}
       </div>
+      <Modal isOpen={isModalOpen} setIsOpen={setIsModalOpen}>
+        <div>
+          <span>Hello world</span>
+        </div>
+      </Modal>
     </TopBarContainer>
   );
 }
