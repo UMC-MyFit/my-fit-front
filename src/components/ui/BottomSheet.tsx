08aefd5df8f4e2ac7bbc61b465b32500bda1ce27
potatodev-777 <potatodev777@gmail.com>
2025-07-04 15:35:48 +0900

    before animating

--- src/components/ui/BottomSheet.tsx
+++ src/components/ui/BottomSheet.tsx
@@ -1,5 +1,41 @@
-function BottomSheet() {
-  return <div></div>;
+import { useRef } from "react";
+
+interface BottomSheetProps {
+  isOpen: boolean;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  children: React.ReactNode;
+}
+
+function BottomSheet({ isOpen, setIsOpen, children }: BottomSheetProps) {
+  const bottomSheetRef = useRef<HTMLDivElement>(null);
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+  const handleOutsideClick = (e: React.MouseEvent) => {
+    if (
+      bottomSheetRef.current &&
+      !bottomSheetRef.current.contains(e.target as Node)
+    ) {
+      handleClose();
+    }
+  };
+
+  if (!isOpen) return null;
+
+  return (
+    <div
+      className="fixed inset-0 bg-ct-black-100/50 z-[9999]"
+      onClick={handleOutsideClick}
+    >
+      <div
+        ref={bottomSheetRef}
+        className="fixed bottom-0 left-0 right-0 min-h-[400px] rounded-t-[30px] bg-ct-white ct-center px-[30px] py-[45px]"
+      >
+        {children}
+      </div>
+    </div>
+  );
 }
 
 export default BottomSheet;
--- src/pages/profile/CardDetail.tsx
+++ src/pages/profile/CardDetail.tsx
@@ -2,6 +2,7 @@ import DetailIntroduction from "../../components/profile/DetailIntroduction";
 import DetailCardItem from "../../components/profile/DetailCardItem";
 import TopBarContainer from "../../components/common/TopBarContainer";
 import Modal from "../../components/ui/Modal";
+import BottomSheet from "../../components/ui/BottomSheet";
 import { useState } from "react";
 
 const TopBarContent = () => {
@@ -17,18 +18,24 @@ const TopBarContent = () => {
 function CardDetail() {
   const createList = Array.from({ length: 10 }, (_, i) => i + 1);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isBottomSheetOpen, setIsBottomSheetOpen] = useState(false);
 
   return (
     <TopBarContainer TopBarContent={<TopBarContent />}>
-      <div className="w-full h-[100px]" onClick={() => setIsModalOpen(true)}>
-        open modal
-      </div>
       <div className="w-full h-full bg-ct-gray-100 flex flex-col gap-[7px]">
         <DetailIntroduction />
         {createList.map((_, index) => (
-          <DetailCardItem key={index} />
+          <DetailCardItem
+            key={index}
+            setIsBottomSheetOpen={setIsBottomSheetOpen}
+          />
         ))}
       </div>
+      <BottomSheet isOpen={isBottomSheetOpen} setIsOpen={setIsBottomSheetOpen}>
+        <div>
+          <span>Hello world</span>
+        </div>
+      </BottomSheet>
       <Modal isOpen={isModalOpen} setIsOpen={setIsModalOpen}>
         <div>
           <span>Hello world</span>
