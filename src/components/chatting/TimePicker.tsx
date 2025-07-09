cd24951c52ba64e2a5f6e9729fd5c1167accbe52
potatodev-777 <potatodev777@gmail.com>
2025-07-09 14:51:20 +0900

    feat:coffeechat-request

--- src/components/chatting/TimePicker.tsx
+++ src/components/chatting/TimePicker.tsx
@@ -3,7 +3,7 @@ import TimeBox from "./TimeBox";
 
 function TimePicker() {
   return (
-    <div className="w-[330px]">
+    <div className="w-[330px] mb-[52px]">
       <div className="mb-[10px]">
         <span className="text-body1 text-ct-gray-400">오전</span>
         <div className="grid grid-cols-5 gap-2 mt-[13px]">
