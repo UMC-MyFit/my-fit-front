b534110c6520637c3737de2d2af8e6f043aa9809
simonnchaser <jico9954@gmail.com>
2025-07-17 16:56:50 +0900

    personalprofile 로직함께 수정

--- src/pages/onboarding/ProfileRegister.tsx
+++ src/pages/onboarding/ProfileRegister.tsx
@@ -9,6 +9,7 @@ import BirthModal from "../../components/onboarding/BirthModal";
 import RegionModal from "../../components/onboarding/RegionModal";
 import SubRegionModal from "../../components/onboarding/SubRegionModal";
 import { useLocation, useNavigate } from "react-router-dom";
+import BottomCTAButton from "../../components/common/BottomCTAButton";
 
 function ProfileRegister() {
   const { isModalOpen, setIsModalOpen } = useModal();
@@ -64,97 +65,121 @@ function ProfileRegister() {
   };
   return (
     <TopBarContainer TopBarContent={<TopBarContent />}>
-      <div className="relative flex flex-col pt-[19px] gap-[27px] mb-[35px] items-center">
+      <div className="relative pt-[19px] pb-[35px]">
         {/* ✅ 스텝 인디케이터 */}
         <div className="absolute top-[0px] right-[22px] flex items-center gap-[6px] z-10">
           <img src="/assets/onboarding/step1.svg" alt="현재 스텝 1" />
           <img src="/assets/onboarding/nonestep.svg" alt="none" />
         </div>{" "}
-        <PersonalInputField
-          label="닉네임"
-          value={nickname}
-          onChange={(e) => setNickname(e.target.value)}
-        />{" "}
-        <div className="flex flex-col gap-[8px]">
+        <div className="w-full max-w-[400px] px-[24px] mx-auto flex flex-col gap-[27px]">
           <PersonalInputField
-            label="한줄 소개"
-            value={shortIntro}
-            onChange={(e) => setShortIntro(e.target.value)}
+            label="닉네임"
+            value={nickname}
+            onChange={(e) => setNickname(e.target.value)}
           />{" "}
-          <span className="text-Body1 text-ct-gray-200 ml-[16px]">
-            한줄로 나에 대해 나타내보세요!
-            <br />
-            EX. 저는 워라밸보다 연봉에 더 욕심이 있어요.
-          </span>
-        </div>
-        <PersonalInputField
-          label="나이"
-          value={birthDate}
-          placeholder="생년월일 입력"
-          onClick={() => openModal("birth")}
-        />
-        <PersonalInputField
-          label="주 활동 지역"
-          value={region}
-          placeholder="주 활동지역 입력"
-          onClick={() => openModal("region")}
-        />
-        <PersonalInputField
-          label="세부 활동 지역"
-          value={subRegion}
-          placeholder="세부 활동지역 입력"
-          onClick={() => {
-            if (!region) {
-              setSubRegionError("먼저 주 활동 지역을 선택해주세요.");
-            } else {
-              setSubRegionError("");
-              openModal("subregion");
-            }
-          }}
-          error={subRegionError}
-        />
-        <PersonalInputField
-          label="현재 구인/구직 상태를 알려주세요!"
-          value={employ}
-          placeholder="구인/구직 상태 입력"
-          onClick={() => openModal("employment")}
-        />
-        <PersonalInputField
-          label="희망 직무를 선택해주세요"
-          value={selectedJob || ""}
-          placeholder="희망직무 입력"
-          onClick={() =>
-            nav("/personalsetting/profile/jobpreference", {
-              state: {
-                from: "onboarding",
-                prevData: {
-                  region,
-                  subRegion,
-                  birthDate,
-                  employ,
-                  academic,
-                  selectedJob,
-                  nickname,
-                  shortIntro,
-                  educationLevel,
+          <div className="flex flex-col gap-[8px]">
+            <PersonalInputField
+              label="한줄 소개"
+              value={shortIntro}
+              onChange={(e) => setShortIntro(e.target.value)}
+            />{" "}
+            <span className="text-Body1 text-ct-gray-200 ml-[16px]">
+              한줄로 나에 대해 나타내보세요!
+              <br />
+              EX. 저는 워라밸보다 연봉에 더 욕심이 있어요.
+            </span>
+          </div>
+          <PersonalInputField
+            label="나이"
+            value={birthDate}
+            placeholder="생년월일 입력"
+            onClick={() => openModal("birth")}
+          />
+          <PersonalInputField
+            label="주 활동 지역"
+            value={region}
+            placeholder="주 활동지역 입력"
+            onClick={() => openModal("region")}
+          />
+          <PersonalInputField
+            label="세부 활동 지역"
+            value={subRegion}
+            placeholder="세부 활동지역 입력"
+            onClick={() => {
+              if (!region) {
+                setSubRegionError("먼저 주 활동 지역을 선택해주세요.");
+              } else {
+                setSubRegionError("");
+                openModal("subregion");
+              }
+            }}
+            error={subRegionError}
+          />
+          <PersonalInputField
+            label="현재 구인/구직 상태를 알려주세요!"
+            value={employ}
+            placeholder="구인/구직 상태 입력"
+            onClick={() => openModal("employment")}
+          />
+          <PersonalInputField
+            label="희망 직무를 선택해주세요"
+            value={selectedJob || ""}
+            placeholder="희망직무 입력"
+            onClick={() =>
+              nav("/personalsetting/profile/jobpreference", {
+                state: {
+                  from: "onboarding",
+                  prevData: {
+                    region,
+                    subRegion,
+                    birthDate,
+                    employ,
+                    academic,
+                    selectedJob,
+                    nickname,
+                    shortIntro,
+                    educationLevel,
+                  },
                 },
-              },
-            })
-          }
-        />
-        <PersonalInputField
-          label="최종 학력을 입력해주세요"
-          value={educationLevel}
-          onChange={(e) => setEducationLevel(e.target.value)}
-          placeholder="최종학력 입력"
-        />
-        <PersonalInputField
-          label="재학/졸업 상태를 입력해주세요"
-          value={academic}
-          placeholder="재학/졸업 상태 입력"
-          onClick={() => openModal("academic")}
-        />
+              })
+            }
+          />
+          <PersonalInputField
+            label="최종 학력을 입력해주세요"
+            value={educationLevel}
+            onChange={(e) => setEducationLevel(e.target.value)}
+            placeholder="최종학력 입력"
+          />
+          <PersonalInputField
+            label="재학/졸업 상태를 입력해주세요"
+            value={academic}
+            placeholder="재학/졸업 상태 입력"
+            onClick={() => openModal("academic")}
+          />
+        </div>
+        <div className="w-full max-w-[400px] px-[24px] mx-auto mt-[32px]">
+          <BottomCTAButton
+            text="다음 단계로 이동"
+            onClick={() => {
+              const profileData = {
+                nickname,
+                shortIntro,
+                region,
+                subRegion,
+                birthDate,
+                employ,
+                academic,
+                educationLevel,
+                selectedJob,
+              };
+              nav("/personalsetting/profile", {
+                state: { profileData },
+              });
+            }}
+          />
+        </div>
       </div>
+
       <Modal>
         {isModalOpen && modalType === "region" && (
           <RegionModal onConfirm={(val) => setRegion(val)} />
