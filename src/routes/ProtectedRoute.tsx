05f474a857a3e26fc922d927ae26c9195602d2d2
potatodev-777 <potatodev777@gmail.com>
2025-07-22 22:57:02 +0900

    before-rebase

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,5 +1,14 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
+
 function ProtectedRoute() {
-  return <div>ProtectedRoute</div>;
+  const { isLoggedIn } = useAuth();
+
+  if (isLoggedIn) {
+    return <Outlet />;
+  }
+
+  return <Navigate to="/login" replace />;
 }
 
 export default ProtectedRoute;
