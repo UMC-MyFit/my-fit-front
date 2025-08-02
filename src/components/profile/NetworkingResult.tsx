4f5f1393dc829fbefa0c6a0d8d0d4b2b55357f4a
potatodev-777 <potatodev777@gmail.com>
2025-08-03 00:35:37 +0900

    before2

--- src/components/profile/NetworkingResult.tsx
+++ src/components/profile/NetworkingResult.tsx
@@ -14,11 +14,18 @@ type NetworkingResultProps = {
     | "receivedInterest";
 };
 function NetworkingResult({ selectedTab }: NetworkingResultProps) {
-  const { data: {result: network} = {result: []}, isLoading: networkLoading } = useGetMyNetwork();
-  const { data: {result: receivedNetwork} = {result: []}, isLoading: receivedNetworkLoading } =
-    useGetReceivedNetwork();
-  const { data: {result: receivedInterests} = {result: []}, isLoading: receivedInterestsLoading } =
-    useGetPeopleWhoInterestMe();
+  const {
+    data: { result: network } = { result: [] },
+    isLoading: networkLoading,
+  } = useGetMyNetwork();
+  const {
+    data: { result: receivedNetwork } = { result: [] },
+    isLoading: receivedNetworkLoading,
+  } = useGetReceivedNetwork();
+  const {
+    data: { result: receivedInterests } = { result: [] },
+    isLoading: receivedInterestsLoading,
+  } = useGetPeopleWhoInterestMe();
   const { data: myInterests, isFetching: myInterestsFetching } =
     useGetMyInterest();
 
@@ -37,18 +44,30 @@ function NetworkingResult({ selectedTab }: NetworkingResultProps) {
     network: {
       data: network,
       message: "새로운 네트워크 관계를 만들어 보세요!",
+      name: "other_service_name",
+      profile_img: "other_service_profile_img",
+      sector: "other_service_sector",
     },
     receivedNetwork: {
       data: receivedNetwork,
       message: "받은 요청이 없습니다.",
+      name: "sender_service_name",
+      profile_img: "sender_service_profile_img",
+      sector: "sender_service_sector",
     },
     sendInterest: {
       data: myInterestsData,
       message: "보낸 관심이 없습니다.",
+      name: "recipient_service_name",
+      profile_img: "recipient_service_profile_img",
+      sector: "recipient_service_sector",
     },
     receivedInterest: {
       data: receivedInterests,
       message: "받은 관심이 없습니다.",
+      name: "sender_id",
+      profile_img: "sender_profile_img",
+      sector: "sender_sector",
     },
   };
 
@@ -56,9 +75,29 @@ function NetworkingResult({ selectedTab }: NetworkingResultProps) {
     <>
       <div className="w-full h-auto">
         {matchingData[selectedTab].data?.length ? (
-          
+          matchingData[selectedTab].data.map((user) => {
+            const { name, profile_img, sector } = matchingData[selectedTab];
+
+            return (
+              <div key={user.id} className="ml-2 flex items-center gap-4">
+                <img
+                  src={user[profile_img]}
+                  alt="프로필 이미지"
+                  className="w-12 h-12 rounded-full object-cover"
+                />
+                <div className="flex flex-col">
+                  <span className="font-medium text-base text-black">
+                    {user[name]}
+                  </span>
+                  <span className="text-sm text-gray-500">{user[sector]}</span>
+                </div>
+              </div>
+            );
+          })
         ) : (
-          
+          <p className="text-body2 text-ct-gray-200 text-center">
+            {matchingData[selectedTab].message}
+          </p>
         )}
       </div>
       {!isReady && (
