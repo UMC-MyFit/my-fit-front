513fc93808f5db9497db197cffdae5e21d3ab057
Minsu Kwon <kms37480@naver.com>
2025-08-09 19:03:40 +0900

    before/rebase

--- src/libs/socket.ts
+++ src/libs/socket.ts
@@ -1,8 +1,16 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://3.36.59.150:3000", {
+const socket = io("http://192.168.0.8:3000", {
   transports: ["websocket"],
   withCredentials: true,
+});
+
+socket.on("connect", () => {
+  console.log("✅ socket 연결 성공:", socket.id);
+});
+
+socket.on("connect_error", (err) => {
+  console.error("❌ socket 연결 실패:", err.message);
 });
 
 export default socket;
