43ddb0bbf658b76798edfc99d0f6942dd59a020b
potatodev-777 <potatodev777@gmail.com>
2025-08-11 16:32:19 +0900

    router

---
+++ src/components/profile/ProfileAddCard.tsx
@@ -0,0 +1,9 @@
+function ProfileAddCard() {
+  return (
+    <div className="w-[174px] h-[216px] rounded-[5px] bg-ct-gray-100 ct-center">
+      <img src="/assets/onboarding/plus.svg" alt="plus" />
+    </div>
+  );
+}
+
+export default ProfileAddCard;
---
+++ src/components/profile/ProfileAddFeed.tsx
@@ -0,0 +1,9 @@
+function ProfileAddFeed() {
+  return (
+    <div className="w-[122px] h-[128px] rounded-[5px] bg-ct-gray-100 ct-center">
+      <img src="/assets/onboarding/plus.svg" alt="plus" />
+    </div>
+  );
+}
+
+export default ProfileAddFeed;
